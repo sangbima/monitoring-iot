9ebfd923e3684690ed2b0d2b0abd3c441c564199
Khairil <sangbima.net@gmail.com>
2023-12-11 20:06:10 +0700

    Setting api node

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -3,13 +3,25 @@
 namespace api\controllers;
 
 use yii\rest\Controller;
+use yii\web\Response;
+use yii\helpers\ArrayHelper;
 
 /**
  * Site controller
  */
-class SiteController extends Controller {
+class SiteController extends Controller
+{
+    // public function behaviors()
+    // {
+    //     $behaviors = parent::behaviors();
+    //     $behaviors['contentNegotiator']['formats'] = [
+    //         'application/json' => \yii\web\Response::FORMAT_JSON,
+    //     ];
+    //     return $behaviors;
+    // }
 
-    public function actionIndex() {
+    public function actionIndex()
+    {
         return [
             "data" => "Data Site API"
         ];
