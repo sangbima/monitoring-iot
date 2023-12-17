e864ac3ac96f1b31f10e5dc4564281b61318fe07
Khairil <sangbima.net@gmail.com>
2023-12-17 13:52:43 +0700

    Remove login fb and gmail on login page

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -29,21 +29,6 @@ $this->title = 'Login';
             <div class="d-grid py-3 mt-4">
                 <?= Html::submitButton('Login', ['class' => 'btn btn-lg btn-primary', 'name' => 'login-button']) ?>
             </div>
-
-            <div class="text-center py-3">or Login with</div>
-            <div class="d-flex gap-2 justify-content-center">
-                <button type="submit" class="btn btn-outline-danger">
-                    <i class="bi bi-google me-2"></i>Gmail
-                </button>
-                <button type="submit" class="btn btn-outline-info">
-                    <i class="bi bi-facebook me-2"></i>Facebook
-                </button>
-            </div>
-            <div class="text-center pt-4">
-                <span>Not registered?</span>
-                <a href="signup.html" class="text-blue text-decoration-underline ms-2">
-                    SignUp</a>
-            </div>
         </div>
     </div>
     <?php ActiveForm::end(); ?>
